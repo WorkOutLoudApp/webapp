39807fda82585e7749ae06bf3985081659c7fe86
personallyvnguyen <60557484+personallyvnguyen@users.noreply.github.com>
2023-03-09 19:50:14 -0800

    Added integration with backend (#2)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
+  env: {
+    apiUrl: process.env.NODE_ENV == 'production' || process.env.APP_ENV == 'production' ? 'https://workoutloud.me' : 'http://localhost:3000',
+  },
 }
