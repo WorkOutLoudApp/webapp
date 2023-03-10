7370e283a672b72602120d2f58fb4fb350f76566
personallyvnguyen <60557484+personallyvnguyen@users.noreply.github.com>
2023-03-09 19:49:56 -0800

    Added integration with backend

---
+++ pages/helloworld.js
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+import React, { useState, useEffect } from 'react'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    console.log(process.env.apiUrl)
+    fetch(`${process.env.apiUrl}/api/v1/example/hello`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>WorkOutLoud</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+  
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Response from workoutloud.me/api/v1/helloworld
+          </h1>
+  
+          <p className={styles.description}>
+            <code className={styles.code}>{items.message}</code>
+          </p>
+        </main>
+      </div>
+    )
+  }
+
+  
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>WorkOutLoud</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
+          Welcome to <a href="https://workoutloud.me">WorkOutLoud</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Go to <Link href="/helloworld">/helloworld</Link>.
         </p>
 
         <div className={styles.grid}>
